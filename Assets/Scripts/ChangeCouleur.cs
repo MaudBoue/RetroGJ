b3bc5601bf0bd90d22c8af02c635dcdc215561ca
maudB <maud.boue@gmail.com>
2015-11-14 16:34:13 +0100

    sprites + debut scene

---
+++ Assets/Scripts/ChangeCouleur.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeCouleur : MonoBehaviour {
+
+	private SpriteRenderer rend;
+	public string color;
+
+	public SpriteRenderer Parent;
+
+	// Use this for initialization
+	void Start () {
+		rend = this.GetComponent<SpriteRenderer> ();
+		if (Parent) {
+			rend.color = Parent.color;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
