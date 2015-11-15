ee9e9aac2b5ef2b9c2081b3c39ba353feb3878c7
maudB <maud.boue@gmail.com>
2015-11-15 13:14:03 +0100

    Ecran Game over brouillon

---
+++ Assets/UIGameOver.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIGameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void NewGame(){
+		Application.LoadLevel ("BPlaneteBleue");
+	}
+
+	public void quitter(){
+		Application.Quit();
+	}
+
+
+}
