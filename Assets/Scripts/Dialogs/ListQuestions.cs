73298512801504144a3eb4346bda4352e2d6c0bb
chymz <simon.babay@gmail.com>
2015-11-15 17:50:37 +0100

    Manipuler / Santé

--- Assets/Scripts/Dialogs/ListQuestions.cs
+++ Assets/Scripts/Dialogs/ListQuestions.cs
@@ -29,13 +29,68 @@ public class ListQuestions : MonoBehaviour {
 	private JSONNode actions;
 	private JSONNode dialogs;
 
+	private int countQuestion = 0;
+
+	//private ArrayList allObjets;
+	private ArrayList objets;
+	private string currentInv;
+
+	private Dictionary<string, Objet> allObjets = new Dictionary<string, Objet>();
+
 	void Awake(){
 		actions = Globals.getJSON ("actions.json");
-		dialogs = Globals.getJSON ("tribu1.json");
+
+		allObjets.Add ("Fruit bizarre", new Objet("eat", 25));
+		allObjets.Add ("Capsule de soin", new Objet("eat", 75));
+
+		allObjets.Add ("Bidon de kerozene", new Objet("drink", 50));
+		allObjets.Add ("Bave de limace", new Objet("drink", 25));
+		allObjets.Add ("Flasque de bave", new Objet("drink", 50));
+	
+		allObjets.Add ("Bidon de kerozene vide", new Objet("useless", 0));
+		allObjets.Add ("Idole galactique", new Objet("useless", 0));
+		allObjets.Add ("Bibelot informe", new Objet("useless", 0));
+		allObjets.Add ("Crane usage", new Objet("useless", 0));
+		allObjets.Add ("Globe occulaire", new Objet("useless", 0));
+
+		foreach(KeyValuePair<string, Objet> item in allObjets){
+			item.Value.name = item.Key;
+		}
 	}
 
 	// Use this for initialization
 	void Start () {
+
+		objets = new ArrayList ();
+
+		Debug.Log (Globals.currentPlanetId);
+
+		if (Globals.currentPlanet.tribuId > 0) {
+			dialogs = Globals.getJSON ("tribu" + Globals.currentPlanet.tribuId + ".json");
+		} else {
+			// Random de rencontre
+			if( Random.Range(1,5) == 1 ){
+				// Monters @todo
+			}
+
+			//if( Random.Range (1,3) == 1 ){
+				int randObjCount = Random.Range(1, 3);
+				for(int i = 0; i < randObjCount; i++){
+					// Objets dans la scene
+					int objIndex = Random.Range(0, allObjets.Count);
+					//objets.Add( allObjets[objIndex] );
+					
+					int iobj = 0;
+					foreach(KeyValuePair<string, Objet> item in allObjets){
+						if( iobj == objIndex ){
+							objets.Add( item.Value.name );
+						}
+						iobj++;
+					}
+				}
+			//}
+		}
+
 		Parse (actions.AsObject);
 		UpdateList ();
 		OnChange (0);
@@ -63,6 +118,21 @@ public class ListQuestions : MonoBehaviour {
 
 		int i = 0;
 		foreach ( KeyValuePair<string, JSONNode> item in list) {
+
+			if( (item.Key == "Dire" || item.Key == "Donner") && Globals.currentPlanet.tribuId == 0){
+				continue;
+			}
+
+			if( objets.Count == 0 ){
+				if( item.Key == "Prendre" ){
+					continue;
+				}
+
+				if( Globals.currentPlanet.tribuId == null && item.Key == "Manipuler" ){
+					continue;
+				}
+			}
+
 			currentList[i] = item.Key;
 			currentListValues[i] = item.Value;
 			i++;
@@ -111,6 +181,11 @@ public class ListQuestions : MonoBehaviour {
 				if( textObject != null ){
 					Text text = textObject.GetComponent<Text>();
 					text.text = "";
+
+					if( i == 0 ){
+						text.text = " - Rien - ";
+					}
+
 					text.color = normalColor;
 				}
 			}
@@ -143,13 +218,17 @@ public class ListQuestions : MonoBehaviour {
 	void OnSelectItem(){
 
 		JSONNode item = currentListValues [selected];
-		JSONClass obj = item.AsObject;
+		JSONClass obj = null;
+
+		if(item != null) obj = item.AsObject;
+
+		bool noReset = false;
 
 		if (obj == null) {
 			string str = item;
 
 			// Do
-			if( str.Contains("do:") ){
+			if( str != null && str.Contains("do:") ){
 				str = str.Replace("do:", "");
 				str = str.Replace("\"", "");
 				
@@ -159,67 +238,156 @@ public class ListQuestions : MonoBehaviour {
 				}
 			
 			// Action
-			}else{
+			}else if( str != null && str.Contains("action:")){
 				str = str.Replace("action:", "");
 				str = str.Replace("\"", "");
 
 				currentAction = str;
-				Parse (dialogs.AsObject);
+				Parse (dialogs["questions"].AsObject);
 				UpdateList ();
 				OnChange (0);
+
+			// Inventaire
+			}else{
+
+				switch(currentInv){
+
+					// Prendre un objet au sol
+					case "take":
+
+					Globals.items.Add( currentList[selected] );
+
+					for(int i = 0; i < objets.Count; i++){
+						if( objets[i] == currentList[selected] ){
+							objets.RemoveAt(i);
+							break;
+						}
+					}
+					
+					take ();
+
+					break;
+
+					// Boire
+					case "drink":
+					case "eat":
+
+					for(int i = 0; i < Globals.items.Count; i++){
+						string itemName = (string)Globals.items[i];
+						Objet def = allObjets[itemName];
+
+						if( def.name == currentList[selected] ){
+							Globals.playerHealth += def.score;
+							Globals.items.RemoveAt(i);
+							break;
+						}
+					}
+
+					MethodInfo method = this.GetType().GetMethod( currentInv );
+					method.Invoke(this, null);
+
+					break;
+
+					// Donner
+					case "give":
+
+
+
+					break;
+				}
+
 			}
 
 		} else {
+
 			if( currentAction != "dialogs" ){
 				// Update de la liste avec les éléments enfants
 				Parse (obj);
 				UpdateList ();
 				OnChange (0);
+
 			}else{
+
+				// Affiche la réponse
 				if( !inReponse ){
 
+					countQuestion++;
+
 					JSONArray reponses = currentListValues[selected]["reponses"].AsArray;
+					JSONArray aggrs = currentListValues[selected]["aggr"].AsArray;
 					int att = currentListValues[selected]["attitude"].AsInt;
+
+					ArrayList reponsesFinal = new ArrayList();
+
+					if( true /*Globals.currentPlanet.tribuAttitude < 0*/ ){
+						int count = 0;
+						for(int i = 0; i < aggrs.Count; i++){
+							int aggrId = aggrs[i].AsInt;
+
+							JSONArray reps = dialogs["aggr"][aggrId-1].AsArray;
+
+							for(int j = 0; j < reps.Count; j++){
+								reponsesFinal.Add(reps[j] + "");
+								count++;
+							}
+						}
+					}else{
+						for(int i = 0; i < reponses.Count; i++){
+							reponsesFinal.Add(reponses[i] + "");
+						}
+					}
 
 					// Atitude négative
 					if( att != 0){
 
-						if( Globals.currentPlanet != null && Globals.currentPlanet.tribuAttitude == 0)
-							Globals.currentPlanet.tribuAttitude = att;
+						if( Globals.currentPlanet != null && Globals.currentPlanet.tribuAttitude == 0){
+							//Globals.currentPlanet.tribuAttitude = att;
+							Debug.Log ("Agressif");
+						}
 					
 					// Attitude neutre
 					}else{
 						if( Random.Range(1, 10) == 1 ){
-							Globals.currentPlanet.tribuAttitude = -1;
+							//Globals.currentPlanet.tribuAttitude = -1;
+							Debug.Log ("Top neutre y'en a marre");
+						}
+
+						if( countQuestion == 0 && att == -2){
+							// Fuite
+							Debug.Log ("Fuite");
 						}
 					}
 
-					int randRep = Random.Range(0, reponses.Count);
+					Random.seed = (int)(Time.time * 10000);
+					int randRep = Random.Range(0, reponsesFinal.Count);
 
 					reponseObject.SetActive(true);
 					GameObject reponseText = GameObject.Find ("Reponse");
 					Text t = reponseText.GetComponent<Text>();
-					t.text = reponses[randRep];
+
+					t.text = (string)reponsesFinal.ToArray()[randRep];
 
 					inReponse = true;
+					noReset = true;
+
+				// Fin de réponse
 				}else{
 					reponseObject.SetActive(false);
-
 					inReponse = false;
+					noReset = true;
 				}
 			}
 		}
 
 		// Reset de la selection
-		selected = 0;
-		realSelected = 0;
-		skip = 0;
-
-		// 
+		if (!noReset) {
+			selected = 0;
+			realSelected = 0;
+			skip = 0;
+		}
 	}
 
 	void OnBack(){
-
 		switch(currentAction){
 		default:
 		case "dialogs":
@@ -235,6 +403,76 @@ public class ListQuestions : MonoBehaviour {
 	public void leave(){
 		// Carte de la galaxie
 		Application.LoadLevel("Galaxie");
+	}
+
+	public void take(){
+		currentInv = "take";
+
+		if (objets.Count > 0)
+			ParseGround ();
+		else
+			Parse (actions.AsObject);
+
+		ResetList ();
+	}
+
+	public void give(){
+		currentInv = "give";
+		ParseInv ();
+		ResetList ();
+	}
+
+	public void sleep(){
+		//Globals.playerHealth = 100;
+	}
+
+	public void eat(){
+		currentInv = "eat";
+		ParseInv ();
+		ResetList ();
+	}
+
+	public void drink(){
+		currentInv = "drink";
+		ParseInv ();
+		ResetList ();
+	}
+
+	void ParseInv(){
+		currentList = new string[Globals.items.Count];
+		currentListValues = new JSONNode[Globals.items.Count];
+		
+		for(int i = 0; i < Globals.items.Count; i++){
+
+			string item = (string)Globals.items[i];
+
+			if( currentInv == "eat" || currentInv == "drink" ){
+				Objet def = allObjets[item];
+				if( def.type != currentInv ) continue;
+			}
+
+			currentList[i] = item;
+			currentListValues[i] = null;
+		}
+	}
+
+	void ParseGround(){
+		currentList = new string[objets.Count];
+		currentListValues = new JSONNode[objets.Count];
+
+		for(int i = 0; i < objets.Count; i++){
+			currentList[i] = objets[i].ToString();
+			currentListValues[i] = null;
+		}
+	}
+
+	void ResetList(){
+		selected = 0;
+		realSelected = 0;
+		skip = 0;
+		
+		UpdateList ();
+		OnChange (0);
 	}
 
 }
--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using SimpleJSON;
 
 public class Globals : MonoBehaviour {
 
 	public static PlanetInfo[] planets;
 	public static PlanetInfo currentPlanet;
+	public static int currentPlanetId = 0;
+
+	public static int playerHealth = 100;
+	public static ArrayList items = new ArrayList();
 
 	public static JSONNode getJSON(string filename){
 		return JSON.Parse(System.IO.File.ReadAllText(filename));
