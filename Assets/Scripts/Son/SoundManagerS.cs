c59cb77e14576dcf56ff7ec8fee78a6aa988a904
maudB <maud.boue@gmail.com>
2015-11-15 17:54:40 +0100

    musique

---
+++ Assets/Scripts/Son/SoundManagerS.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManagerS : MonoBehaviour {
+
+	public static SoundManagerS instance = null;
+	private AudioSource AudioS;
+
+	public AudioClip musiquePlanete;
+	public AudioClip musiqueCombat;
+
+	public bool fight = false;
+	// Use this for initialization
+
+	void Awake () {
+		if (instance == null){
+			instance = this;
+		}else if (instance != this)
+			Destroy (gameObject);
+		
+		DontDestroyOnLoad (gameObject);
+	}
+
+
+	void Start () {
+		AudioS = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey(KeyCode.Z)) {
+			fight=true;
+			AudioS.clip = musiqueCombat;
+			AudioS.Play ();
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			fight=false;
+			AudioS.clip = musiquePlanete;
+			AudioS.Play ();
+		}
+
+	}
+}
